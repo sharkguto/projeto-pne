674c7854d313b697b73d60d535b308fe3ee94a71
Felipe Feber <felipe.feber@deviseit.com.br>
2020-06-13 17:01:55 -0300

    Database Update

--- backend/pe-na-estrada-api/Models/TblPointStop.cs
+++ backend/pe-na-estrada-api/Models/TblPointStop.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using pe_na_estrada_api.Interfaces;
 
-namespace pe_na_estrada_api
+namespace pe_na_estrada_api.Models
 {
-    public partial class TblPointStop : IGenericModel
+    public partial class TblPointStop:IGenericModel
     {
+        public TblPointStop()
+        {
+            TblPointStopReview = new HashSet<TblPointStopReview>();
+        }
+
         public long Id { get; set; }
         public double LatPoint { get; set; }
         public double LongPoint { get; set; }
         public string StopOptionsTags { get; set; }
         public string ReviewsAvg { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public virtual ICollection<TblPointStopReview> TblPointStopReview { get; set; }
     }
 }
--- backend/pe-na-estrada-api/Models/TblTrip.cs
+++ backend/pe-na-estrada-api/Models/TblTrip.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using pe_na_estrada_api.Interfaces;
 
-namespace pe_na_estrada_api
+namespace pe_na_estrada_api.Models
 {
-    public partial class TblTrip
+    public partial class TblTrip:IGenericModel
     {
+        public TblTrip()
+        {
+            TblRankingPx = new HashSet<TblRankingPx>();
+            TblTripStop = new HashSet<TblTripStop>();
+        }
+
         public long Id { get; set; }
-        public double LatBegin { get; set; }
-        public double LongBegin { get; set; }
-        public double LatEnd { get; set; }
-        public double LongEnd { get; set; }
         public long IdUser { get; set; }
         public DateTime CreatedAt { get; set; }
+        public string Origin { get; set; }
+        public string Destiny { get; set; }
+        public DateTime? FinishedAt { get; set; }
+        public TimeSpan? MinutesTripAt { get; set; }
 
         public virtual TblUser IdUserNavigation { get; set; }
+        public virtual ICollection<TblRankingPx> TblRankingPx { get; set; }
+        public virtual ICollection<TblTripStop> TblTripStop { get; set; }
     }
 }
