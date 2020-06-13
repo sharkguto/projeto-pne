6b1a825fc4c28098208e4d4edfb413f7ed283e28
Gustavo Freitas <gustavo@gmf-tech.com>
2020-06-13 01:50:09 -0300

    remove public

--- devops/sql-scripts/dump-postgres.sql
+++ devops/sql-scripts/dump-postgres.sql
@@ -18,13 +18,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 4 (class 2615 OID 2200)
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
-CREATE SCHEMA public;
-
 
 --
 -- TOC entry 3050 (class 0 OID 0)
