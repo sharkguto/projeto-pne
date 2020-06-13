54406453fa0ecbf3c83dd3fd4b6b55ca606fc2ee
Felipe Feber <felipe.feber@deviseit.com.br>
2020-06-13 18:42:54 -0300

    Update

--- backend/pe-na-estrada-api/Controllers/UserController.cs
+++ backend/pe-na-estrada-api/Controllers/UserController.cs
@@ -13,5 +13,18 @@ namespace pe_na_estrada_api.Controllers
     [Route("user")]
     public class UserController : GenericController<TblUser, User>
     {
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<TblUser>> Login([FromServices] pneContext pContext, [FromBody] TblUser pUser)
+        {
+            return await new User().Login(pContext.TblUser, pUser.Cellphone, pUser.Password);
+        }
+
+        [HttpPost]
+        [Route("forgot")]
+        public async Task<ActionResult<TblUser>> ForgotPassword([FromServices] pneContext pContext, [FromBody] TblUser pUser)
+        {
+            return await new User().ForgotPassword(pContext.TblUser, pUser.Cellphone, pUser.Cpf, (DateTime)pUser.Birthday);
+        }
     }
 }
