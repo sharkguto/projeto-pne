034d3a67dd6a51346347c8b7c0f2862bf1bbe87e
Gustavo Freitas <gustavo@gmf-tech.com>
2020-06-14 11:31:13 -0300

    dotnet fixed cors

--- backend/pe-na-estrada-api/Models/Ranking.cs
+++ backend/pe-na-estrada-api/Models/Ranking.cs
@@ -4,9 +4,10 @@ using pe_na_estrada_api.Interfaces;
 
 namespace pe_na_estrada_api.Models
 {
-    public partial class Ranking 
+    public partial class Ranking
     {
         public string Nickname { get; set; }
+        public string Name { get; set; }
         public decimal Points { get; set; }
         public DateTime LastActiveDate { get; set; }
 
--- frontend/ranking-px/src/views/Ranking.vue
+++ frontend/ranking-px/src/views/Ranking.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RankingPx msg="Ranking PX - interligando a comunidade de caminhoneiros pelo Brasil"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import RankingPx from '@/components/RankingPx.vue';
 
 export default {
-  name: 'Home',
+  name: 'Ranking',
   components: {
-    HelloWorld,
+    RankingPx,
   },
 };
 </script>
