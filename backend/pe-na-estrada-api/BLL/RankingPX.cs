5deb64fc26e25768032d167c093e949ecbc4a9e6
Gustavo Freitas <gustavo@gmf-tech.com>
2020-06-14 01:36:55 -0300

    super ranking list

--- backend/pe-na-estrada-api/BLL/RankingPX.cs
+++ backend/pe-na-estrada-api/BLL/RankingPX.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using pe_na_estrada_api.Models;
+using System.Data;
+using System;
 
 namespace pe_na_estrada_api.BLL
 {
@@ -11,6 +13,37 @@ namespace pe_na_estrada_api.BLL
         public async Task<List<TblRankingPx>> GetPointsByTripId(DbSet<TblRankingPx> pTable, int iTripId)
         {
             return await pTable.Where(x => x.IdTrip == iTripId).ToListAsync();
+        }
+
+        public async Task<List<Ranking>> GetCalculatedRanking(pneContext pContext)
+        {
+            string sQuery = "select us.nickname as nickname, sum (trp.points ) as points, " +
+            "max(trp.created_at ) as last_active_date " +
+            "from tbl_ranking_px trp " +
+            "inner join tbl_user us on trp.id_user =us.id " +
+            "group by us.nickname";
+
+            List<Ranking> pRankingList = new List<Ranking>();
+            using (var command = pContext.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = sQuery;
+                pContext.Database.OpenConnection();
+
+                using (var result = await command.ExecuteReaderAsync())
+                {
+                    while (result.Read())
+                    {
+                        Ranking pRanking = new Ranking();
+                        pRanking.Nickname = result["nickname"].ToString();
+                        pRanking.Points = (decimal)result["points"];
+                        pRanking.LastActiveDate = (DateTime)result["last_active_date"];
+
+                        pRankingList.Add(pRanking);
+                    }
+
+                }
+            }
+            return pRankingList;
         }
     }
 }
