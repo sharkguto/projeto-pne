22b8727f9268a17f65e50c5fb762aad4020aeb8d
Gustavo Freitas <gustavo@gmf-tech.com>
2020-06-14 16:10:20 -0300

    ajuste ranking

---
+++ frontend/ranking-px/src/components/DetailsPx.vue
@@ -0,0 +1,77 @@
+<template>
+<div>
+<section class="hero ccr-vermelho">
+  <div class="hero-body">
+    <div class="container">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <h1 class="title">
+        {{ title }}
+      </h1>
+      <h2 class="subtitle">
+        {{subTitle}}
+      </h2>
+    </div>
+  </div>
+</section>
+<section>
+  <div class="container" v-if="listRankingPx">
+    {{listRankingPx}}
+  </div>
+</section>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'DetailsPx',
+  props: {
+    title: String,
+    subTitle: String,
+  },
+  created() {
+    axios
+      .get('http://localhost:8080/ranking-px/list', null, {
+        headers: { 'x-api-key': 'token-auth-here' },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          this.listRankingPx = response.data;
+        }
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          this.logout();
+        }
+      });
+  },
+  data() {
+    return {
+      listRankingPx: null,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.ccr-vermelho {
+  background-color: #CF2A27;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ frontend/ranking-px/src/views/Details.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <DetailsPx title="Ranking PX"
+    subTitle="Interligando a comunidade de caminhoneiros pelo Brasil"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DetailsPx from '@/components/DetailsPx.vue';
+
+export default {
+  name: 'Details',
+  components: {
+    DetailsPx,
+  },
+};
+</script>
