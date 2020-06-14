24f81fcecfb900c78e43d62f6a5cd9b56df4f771
Felipe Feber <felipe.feber@deviseit.com.br>
2020-06-14 19:59:20 -0300

    fix

--- app/src/pages/Stretching/index.tsx
+++ app/src/pages/Stretching/index.tsx
@@ -91,7 +91,7 @@ const Stretching = () => {
       <ScrollView style={styles.viewSize}>
         <View style={styles.header}>
           <TouchableOpacity onPress={handleNavigateBack}>
-            <Icon name="arrow-left" size={25} color="#000" />
+            <Icon name="arrow-left" size={48} color="#000" />
           </TouchableOpacity>
           <Title>Alogamento</Title>
           <Text></Text>
